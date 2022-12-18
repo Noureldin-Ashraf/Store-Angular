5b3637f6fb28414c0944becc31b0cb1e9a617f6f
Nour Eldin Ashraf <nour.eldin.ashraf05@gmail.com>
2022-12-19 00:16:14 +0200

    Project scaffolding
    - Generated project components
    - Added provided JSON file to assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductItemDetailComponent } from './product-item-detail/product-item-detail.component';
+import { CartComponent } from './cart/cart.component';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductItemComponent,
+    ProductItemDetailComponent,
+    CartComponent,
+    ConfirmationComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
