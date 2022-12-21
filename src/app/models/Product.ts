9853952f7a1fe7f2f7a189fba72aff9256352bf7
Nour Eldin Ashraf <nour.eldin.ashraf05@gmail.com>
2022-12-21 19:43:15 +0200

    Model and service
    - Created Model for products
    - Created Service for products

---
+++ src/app/models/Product.ts
@@ -0,0 +1,16 @@
+export class Product{
+    id:number;
+    name:string;
+    description:string;
+    price:number;
+    url:string;
+
+    constructor(){
+        this.id = 1;
+        this.name ='';
+        this.description = '';
+        this.price = 1;
+        this.url='';
+    }
+}
+
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { Product } from '../models/Product';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+
+  products: Product[] = [];
+
+  constructor(/*private postService: PostService*/){}
+
+  ngOnInit(): void{
+    /*this.postService.getPosts().subscribe(data => {
+      this.posts = data;
+    });*/
+  }
 
 }
